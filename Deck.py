1715b1da046363f5528bba46e7ffc7b2e52b52ef
Mohammed_A <alaadhami@gmail.com>
2019-04-13 19:42:30 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -2,6 +2,9 @@ class Deck:
 # Deck class has attributes:
 
     deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
+    dealerHand = []
+    playerHand = []
+
     def __init__(self, aaa, bbb):
         self.aaa = aaa;
         self.bbb = bbb;
