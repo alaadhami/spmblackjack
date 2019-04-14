fdc81687b92b2f86c1f377d48915bdf151b665c9
Mohammed_A <alaadhami@gmail.com>
2019-04-13 19:39:54 -0500

    Update Dealer.py

--- Dealer.py
+++ Dealer.py
@@ -1,7 +1,33 @@
+import random
+
 class Dealer:
 # Dealer class has attributes:
-
+    hand = []
     def __init__(self, aaa, bbb):
         self.aaa = aaa;
         self.bbb = bbb;
     
+
+    
+    def deal(deck):
+
+        for i in range(2):
+            random.shuffle(deck)
+            card = deck.pop()
+            if card == 11:card = "J"
+            if card == 12:card = "Q"
+            if card == 13:card = "K"
+            if card == 14:card = "A"
+            hand.append(card)
+            return hand
+
+
+
+    def hit(hand):
+        card = deck.pop()
+        if card == 11: card = "J"
+        if card == 12: card = "Q"
+        if card == 13: card = "K"
+        if card == 14: card = "A"
+        hand.append(card)
+        return hand
