80eef71bee3807ef20197f58634b29faaffadd67
Mohammed_A <alaadhami@gmail.com>
2019-04-13 19:16:32 -0500

    Update Deck.py

--- Deck.py
+++ Deck.py
@@ -1,6 +1,7 @@
 class Deck:
 # Deck class has attributes:
 
+    deck = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14] * 4
     def __init__(self, aaa, bbb):
         self.aaa = aaa;
         self.bbb = bbb;
