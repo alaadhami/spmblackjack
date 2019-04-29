e9e858580bd1b93fbee0ddb7835d64ea149ef4fa
JacobV92 <49595253+JacobV92@users.noreply.github.com>
2019-04-29 00:18:50 -0500

    Update Game.py
    added comments

--- Game.py
+++ Game.py
@@ -38,42 +38,43 @@ class Game:
             if p.hand[0].value == p.hand[1].value:
                 split = True;
             
-            if p.getHandValue() != 'Blackjack':
-                in_play = True;
-                first = True;
+            if p.getHandValue() != 'Blackjack': #players don't play if they get blackjack, they just win, unless dealer gets blackjack too
+                in_play = True; #can player still make moves
+                first = True;   #is this the players first move
                 while in_play:
                     print(p.name,'hand:');
                     p.printHand();
-                    if split and not(p.split):
+                    if split and not(p.split): #can the players cards be split
                         print ("can 'split'");
                     play = input("'hit','stand','surrender','double'?:");
-                    if (play == 'stand'):
+                    if (play == 'stand'): #players turns are over when they stand
                         in_play = False;
-                    elif (play == 'hit'):
+                    elif (play == 'hit'): #player choosed to add cards, check if they busted (>21) or reached 21, can no longer play if they did
                         first = False;
                         p.hit(self.deck);
                         v = p.getHandValue();
                         if v == 'Bust' or v == 21:
                             in_play = False;
-                    elif (play == 'double' and first):
+                    elif (play == 'double' and first): #player can double their bet on their first move and get delt one card, then their turn is over, no more hitting, you get one 1 card only
                         p.bet = p.bet*2;
                         p.hit(self.deck);
                         p.printHand();
                         in_play = False;
-                    elif (play == 'surrender' and first):
+                    elif (play == 'surrender' and first): #player can get half their bet back if they don't like their odds, their turns are over after this, like cutting your loses.
                         p.balance -= p.bet/2;
                         p.bet = 0;
                         in_play = False;
-                    elif (play == 'split' and split and not(p.split)):
-                        p.split = True;
-                        p.splitHand.append(p.hand.pop());
-                        p.splitBet = p.bet;
+                    elif (play == 'split' and split and not(p.split)): #if split is true (determined on ln 38 of Game.py) both cards have same value, can turn them into two hands.
+                        p.split = True; #player attribute set to true to indicate player is playing two hands.
+                        p.splitHand.append(p.hand.pop());# pop a card from first hand to the second
+                        p.splitBet = p.bet;# second hand has an additional and equal bet to the first bet
+                        #
             else:
                 print(p.name,'hand:');
                 p.printHand();
             
                         
-        for p in self.players:
+        for p in self.players:#if any player is has a split hand we play it out here with the same rules as before but with no further splitting
             if p.split:
                 in_play = True;
                 first = True;
@@ -102,8 +103,8 @@ class Game:
 
         #Dealer play phase
     def dealerPlayPhase(self):
-        in_play = self.dealer.playoutTick(self.deck);
-        while(in_play):
+        in_play = self.dealer.playoutTick(self.deck); #dealer always takes 1 turn, function returns true if the rules say they need to take more
+        while(in_play):#keep playing untill the rules say the dealer stops, dealer has higher than 16 and it's not soft 17 or they busted out.
             in_play = self.dealer.playoutTick(self.deck);
         print('Dealer Hand:');
         self.dealer.printHand();
@@ -111,32 +112,32 @@ class Game:
         #Payout phase
     def payoutPhase(self):
         dValue, soft = self.dealer.getHandValue();
-        self.dealer.discardHand();
+        self.dealer.discardHand();                  #gets value of dealers hand
         for p in self.players:
-            pValue = p.getHandValue();
+            pValue = p.getHandValue();  #for each player get the value of their hand
             p.discardHand();
-            if pValue == 'Bust':
+            if pValue == 'Bust':    #if player busts player loses, subtract bet from player balance
                 p.balance -= p.bet;
                 print(p.name, 'Lose.');
-            elif dValue == 'Bust':#dealer bust
+            elif dValue == 'Bust':  #if dealer bust and player did not, player wins, add bet to player balance
                 p.balance += p.bet;
                 print(p.name, 'Win.');
-            elif pValue == 'Blackjack' and dValue != 'Blackjack':#player bj dealer no bj
+            elif pValue == 'Blackjack' and dValue != 'Blackjack': #player got blackjack dealer did not, add bet * blackjack bonus value to player balance
                 p.balance += p.bet*self.blackjack;
                 print(p.name, 'BlackJack.');
-            elif dValue == 'Blackjack' and pValue != 'Blackjack':
+            elif dValue == 'Blackjack' and pValue != 'Blackjack': #dealer got blackjack player didn't, normal player lose, subtract bet from balance
                 p.balance -= p.bet;
                 print(p.name, 'Lose.');
-            elif pValue == dValue:
+            elif pValue == dValue:                      #player and dealer tie, player loses nothing
                 print(p.name, 'Push.');
-            elif pValue > dValue:
+            elif pValue > dValue:                       #player wins by having higher value than dealer, add bet to balance
                 p.balance += p.bet;
                 print(p.name, 'Win.');
-            else:
+            else:                                       #process of elimination, the dealer has the higher value, player lost, subtract bet from balance
                 p.balance -= p.bet;
                 print(p.name, 'Lose.');
                 
-        for p in self.players:
+        for p in self.players:                  #same thing again for split hands
             if p.split:
                 p.split = False;
                 pValue = p.getSplitHandValue();
@@ -165,8 +166,8 @@ class Game:
         for p in self.players:
             print(p.name, 'Balance:', p.balance);
 
-    def replay(self):
-        if len(self.deck.cardList) < self.shoeSize/3:
+    def replay(self):                                       #replay all phases of game. skip setup. if 2/3rds of deck has been played, refresh deck
+        if len(self.deck.cardList) < self.shoeSize/3:       #actually don't know if this is a rule but deck is never suposed to run out of cards
             del self.deck;
             self.deck = Deck(self.num_decks);
             print('Refreshing Deck');
